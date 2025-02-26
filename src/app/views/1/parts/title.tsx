b0ae4adc41b680b5e150cec23970cd20d22b2c27
Luke Shiels <47612057+skve@users.noreply.github.com>
2025-02-26 14:33:21 -0800

    1
    Signed-off-by: Luke Shiels <47612057+skve@users.noreply.github.com>

--- src/app/views/1/parts/title.tsx
+++ src/app/views/1/parts/title.tsx
@@ -1,30 +1,30 @@
 "use client";
 
-import { motion,  Variants } from "motion/react";
+import { motion, Variants } from "motion/react";
 
 const containerVariants: Variants = {
   hidden: { opacity: 0 },
   visible: {
     opacity: 1,
     transition: {
-      delay: 1,          // Wait 2 seconds before starting the container animation
-      duration: 1,       // Fade in the container over 1 second
-      when: "beforeChildren", // Children won't start animating until the container finishes
-      staggerChildren: 0.2,   // Stagger the delays for each child
+      delay: 1,
+      duration: 1,
+      when: "beforeChildren",
+      staggerChildren: 0.2,
     },
   },
 };
 
 const childVariants: Variants = {
   hidden: { opacity: 0, y: "20%", filter: "blur(10px)" },
-  visible: { 
-    opacity: 1, 
-    y: 0, 
+  visible: {
+    opacity: 1,
+    y: 0,
     filter: "blur(0px)",
     transition: {
       duration: 1.15,
       ease: [0.25, 0.1, 0.25, 1],
-    }
+    },
   },
 };
 
@@ -35,7 +35,6 @@ export function IntroTitle() {
       initial="hidden"
       animate="visible"
       variants={containerVariants}
-      // transition={containerTransition}
     >
       <motion.h1 className="text-4xl text-gray-12" variants={childVariants}>
         Luke Shiels
