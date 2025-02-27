5b487c3c0a32821b472ca473bbd6a82f3d7a410d
Luke Shiels <47612057+skve@users.noreply.github.com>
2025-02-27 12:09:11 -0800

    improve bg contrast
    Signed-off-by: Luke Shiels <47612057+skve@users.noreply.github.com>

--- src/app/components/slide/slide-container.tsx
+++ src/app/components/slide/slide-container.tsx
@@ -7,7 +7,7 @@ export function SlideContainer({
   return (
     <main
       className={cn(
-        "select-none overflow-hidden bg-gray-1 h-dvh flex justify-center items-center",
+        "select-none overflow-hidden bg-gray-2/70 h-dvh flex justify-center items-center",
         "bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:12px_12px] bg-fixed  [--pattern-fg:var(--black-a3)]"
       )}
       {...props}
