9ace7525724d089fc9f24eb1440aab16e2e73e5c
Luke Shiels <47612057+skve@users.noreply.github.com>
2025-02-27 14:07:22 -0800

    123
    Signed-off-by: Luke Shiels <47612057+skve@users.noreply.github.com>

--- src/app/views/2/page.tsx
+++ src/app/views/2/page.tsx
@@ -1,35 +1,37 @@
+"use client";
+
 import { Grid } from "./grid";
 import { GridItem } from "./grid-item";
 
 export default function View2() {
   return (
-    <div className="overflow-y-auto w-full flex h-full ">
+    <div className="overflow-y-auto w-full flex h-full">
       <Grid>
         <GridItem className="col-span-1 row-span-1">
           <p>Live Dot</p>
-      </GridItem>
-      <GridItem className="row-start-2 col-span-1 row-span-1">
-        <p>???</p>
-      </GridItem>
-      <GridItem className="col-span-1 row-span-2">
-        <p>Tailwind Demo</p>
-      </GridItem>
-      <GridItem className="col-span-2 row-span-1">
-        <p>Suggested Questions?</p>
-      </GridItem>
-      <GridItem className="col-span-2 row-span-1">
-        {/* <p>Voice Memo</p> */}
-        <video
-          autoPlay
-          muted
-          loop
-          playsInline
-          className="w-full h-full object-cover"
-          src="https://rmqdxpokhwuzn5kd.public.blob.vercel-storage.com/work-previews/voice-memo-0HyUjp5JgN13YeHu2TYmdr4cvFwCrs.mp4"
-        ></video>
-      </GridItem>
+        </GridItem>
+        <GridItem className="row-start-2 col-span-1 row-span-1">
+          <p>???</p>
+        </GridItem>
+        <GridItem className="col-span-1 row-span-2">
+          <p>Tailwind Demo</p>
+        </GridItem>
+        <GridItem className="col-span-2 row-span-1">
+          <p>Suggested Questions?</p>
+        </GridItem>
+        <GridItem className="col-span-2 row-span-1">
+          {/* <p>Voice Memo</p> */}
+          <video
+            autoPlay
+            muted
+            loop
+            playsInline
+            className="w-full h-full object-cover"
+            src="https://rmqdxpokhwuzn5kd.public.blob.vercel-storage.com/work-previews/voice-memo-0HyUjp5JgN13YeHu2TYmdr4cvFwCrs.mp4"
+          ></video>
+        </GridItem>
 
-      <GridItem className="col-span-3 row-span-1">
+        <GridItem className="col-span-3 row-span-1">
           <p>T/A Rename</p>
         </GridItem>
       </Grid>
