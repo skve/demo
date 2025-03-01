bcd25dd947816fefc64b0dc5b049e85a039d8955
Luke Shiels <47612057+skve@users.noreply.github.com>
2025-02-28 20:18:06 -0800

    scroll + remove bg
    Signed-off-by: Luke Shiels <47612057+skve@users.noreply.github.com>

--- src/app/components/slide/slide-container.tsx
+++ src/app/components/slide/slide-container.tsx
@@ -8,7 +8,6 @@ export function SlideContainer({
     <main
       className={cn(
         "select-none overflow-hidden bg-gray-2/70 h-dvh flex justify-center items-center",
-        "bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:12px_12px] bg-fixed  [--pattern-fg:var(--black-a3)]"
       )}
       {...props}
     >
