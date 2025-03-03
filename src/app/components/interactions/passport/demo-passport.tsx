48c770a985d2a7afde02191fdbc26e6aee43817a
Luke Shiels <47612057+skve@users.noreply.github.com>
2025-03-02 19:57:47 -0800

    Refactor DemoPassport layout and add mobile bottom bar styling
    Signed-off-by: Luke Shiels <47612057+skve@users.noreply.github.com>

--- src/app/components/interactions/passport/demo-passport.tsx
+++ src/app/components/interactions/passport/demo-passport.tsx
@@ -3,9 +3,13 @@ import { Passport } from "./Passport";
 
 export function DemoPassport() {
   return (
-    <div className="h-fit w-full relative">
-      <Passport />
-      <EncryptionText />
+    <div className="flex-1 h-full relative flex flex-col justify-center items-center">
+      <div className="fixed pointer-events-auto inset-x-0 w-full sm:hidden h-[clamp(5rem,_10vh,_6rem)] z-50 backdrop-blur-sm bottom-0 [mask-image:linear-gradient(to_top,#000_2%,transparent)] before:content-[''] before:absolute before:inset-0 before:bg-[linear-gradient(to_top,var(--gray-2),transparent)]"></div>
+
+      <div className="h-fit w-full top-28 sm:top-[unset] relative">
+        <Passport />
+        <EncryptionText />
+      </div>
     </div>
   );
 }
