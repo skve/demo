8990b2ca3ff1ce090aca70f6224f16760bcec895
Luke Shiels <47612057+skve@users.noreply.github.com>
2025-05-05 08:17:17 -0700

    Fix passport footer formatting by adding a missing delimiter in the passport number string.

--- src/app/components/interactions/passport/PassportFooter.tsx
+++ src/app/components/interactions/passport/PassportFooter.tsx
@@ -1,7 +1,7 @@
 export function PassportFooter() {
   return (
     <footer className="text-[.625rem] font-mono-system mt-auto w-full break-words font-medium leading-4 tracking-wider">
-      {`P<USMIESVANDERROHE<<LUDWIG<<<<<<<<<<<US555555<USA8603270M3401170<<<<<<<<06`}
+      {`P<USMIESVANDERROHE<<LUDWIG<<<<<<<<<<<US555555<USA8603270M3401170<<<<<<<<06<<`}
     </footer>
   );
 }
